0841f221cf67fc9d811080d9b3ef3ac71951c509
Qone2 <whrbdnjs33@gmail.com>
2023-08-28 14:48:35 +0900

    섹션9: 계산 기능 구현

--- 08-practice-project/01-starting-project/src/App.js
+++ 08-practice-project/01-starting-project/src/App.js
@@ -1,8 +1,11 @@
 import Header from "./components/Header/Header";
 import Form from "./components/Form/Form";
 import ResultTable from "./components/ResultTable/ResultTable";
+import { useState } from "react";
 
 function App() {
+  const [result, setResult] = useState([]);
+
   const calculateHandler = (userInput) => {
     // Should be triggered when form is submitted
     // You might not directly want to bind it to the submit event on the form though...
@@ -13,21 +16,27 @@ function App() {
     const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
     const expectedReturn = +userInput["expected-return"] / 100;
     const duration = +userInput["duration"];
+    let totalInterest = 0;
+    let investedCapital = currentSavings;
 
     // The below code calculates yearly results (total savings, interest etc)
     for (let i = 0; i < duration; i++) {
       const yearlyInterest = currentSavings * expectedReturn;
       currentSavings += yearlyInterest + yearlyContribution;
+      totalInterest += yearlyInterest;
+      investedCapital += yearlyContribution;
       yearlyData.push({
         // feel free to change the shape of the data pushed to the array!
         year: i + 1,
         yearlyInterest: yearlyInterest,
         savingsEndOfYear: currentSavings,
-        yearlyContribution: yearlyContribution,
+        totalInterest: totalInterest,
+        investedCapital: investedCapital,
       });
     }
 
     // do something with yearlyData ...
+    setResult(yearlyData);
   };
 
   return (
@@ -39,7 +48,7 @@ function App() {
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
 
-      <ResultTable />
+      <ResultTable result={result} />
     </div>
   );
 }
--- 08-practice-project/01-starting-project/src/components/ResultTable/ResultTable.js
+++ 08-practice-project/01-starting-project/src/components/ResultTable/ResultTable.js
@@ -1,6 +1,46 @@
 import styles from "./ResultTable.module.css";
 
-function ResultTable() {
+function ResultTable(props) {
+  let tbody = (
+    <tbody>
+      <tr>
+        <td>YEAR NUMBER</td>
+        <td>TOTAL SAVINGS END OF YEAR</td>
+        <td>INTEREST GAINED IN YEAR</td>
+        <td>TOTAL INTEREST GAINED</td>
+        <td>TOTAL INVESTED CAPITAL</td>
+      </tr>
+    </tbody>
+  );
+
+  if (props.result.length > 0) {
+    tbody = (
+      <tbody>
+        {props.result.map((e) => (
+          <tr key={e.year}>
+            <td>{e.year}</td>
+            <td>
+              $
+              {e.savingsEndOfYear
+                .toFixed(2)
+                .replace(/\d(?=(\d{3})+\.)/g, "$&,")}
+            </td>
+            <td>
+              ${e.yearlyInterest.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,")}
+            </td>
+            <td>
+              ${e.totalInterest.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,")}
+            </td>
+            <td>
+              $
+              {e.investedCapital.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,")}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    );
+  }
+
   return (
     <table className={styles.result}>
       <thead>
@@ -12,15 +52,7 @@ function ResultTable() {
           <th>Invested Capital</th>
         </tr>
       </thead>
-      <tbody>
-        <tr>
-          <td>YEAR NUMBER</td>
-          <td>TOTAL SAVINGS END OF YEAR</td>
-          <td>INTEREST GAINED IN YEAR</td>
-          <td>TOTAL INTEREST GAINED</td>
-          <td>TOTAL INVESTED CAPITAL</td>
-        </tr>
-      </tbody>
+      {tbody}
     </table>
   );
 }

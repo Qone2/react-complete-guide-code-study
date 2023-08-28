9959d618e6073df3c906cf5b8e62f49f0a347918
Qone2 <whrbdnjs33@gmail.com>
2023-08-28 14:48:13 +0900

    섹션9: 폼 상태관리

--- 08-practice-project/01-starting-project/src/components/Form/Form.js
+++ 08-practice-project/01-starting-project/src/components/Form/Form.js
@@ -1,17 +1,46 @@
 import styles from "./Form.module.css";
 import Button from "../UI/Button";
+import { useState } from "react";
 
-function Form() {
+function Form(props) {
+  const [currentSaving, setCurrentSaving] = useState("");
+  const [yearlyContrib, setYearlyContrib] = useState("");
+  const [expectedReturn, setExpectedReturn] = useState("");
+  const [duration, setDuration] = useState("");
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const userInput = {
+      "current-savings": currentSaving,
+      "yearly-contribution": yearlyContrib,
+      "expected-return": expectedReturn,
+      duration: duration,
+    };
+
+    props.calculateHandler(userInput);
+  }
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <div className={styles["input-group"]}>
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input
+            value={currentSaving}
+            onChange={(event) => setCurrentSaving(event.target.value)}
+            type="number"
+            id="current-savings"
+          />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input
+            type="number"
+            id="yearly-contribution"
+            value={yearlyContrib}
+            onChange={(event) => setYearlyContrib(event.target.value)}
+          />
         </p>
       </div>
       <div className={styles["input-group"]}>
@@ -19,11 +48,21 @@ function Form() {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input
+            type="number"
+            id="expected-return"
+            value={expectedReturn}
+            onChange={(event) => setExpectedReturn(event.target.value)}
+          />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input
+            type="number"
+            id="duration"
+            value={duration}
+            onChange={(event) => setDuration(event.target.value)}
+          />
         </p>
       </div>
       <p className="actions">
